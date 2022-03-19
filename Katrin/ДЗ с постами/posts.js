db622ce634607b48200ccfca2196ead321e863e8
Maxim-AR <96651259+Maxim-AR@users.noreply.github.com>
2022-03-19 18:08:09 +0500

    Delete Katrin/ДЗ с постами directory

--- Katrin/ДЗ с постами/posts.js
+++
@@ -1,4 +0,0 @@
-let a = document.querySelector('.posts')
-fetch(' https://jsonplaceholder.typicode.com/posts')
-.then ((value) => value.json())
-.then((res) => a.innerHTML = (JSON.stringify(res.map(post => `<h2>${post.title}</h2><p>${post.body}</p>`).join('<br>'))));
